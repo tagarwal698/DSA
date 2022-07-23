3ce9997d242655e2b918529f4b89b1431dee4d18
Tushar <tagarwal698@gmail.com>
2022-07-23 23:39:05 +0530

    Basic Array Problems

---
+++ Arrays/pair_sum.cpp
@@ -0,0 +1,123 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ff 					first
+#define ss 					second
+#define int 				long long
+#define ll					long long int
+#define pb 					push_back
+#define mp 					make_pair
+#define pii					pair<int,int>
+#define vi					vector<int>
+#define mii					map<int,int>
+#define pqb					priority_queue<int>
+#define pqs					priority_queue<int,vi,greater<int>>
+#define setbits(x)			__builtin_popcountll(x) //number of setbits
+#define zrobits(x)			__builtin_ctzll(x) //number of zeros before 1 from the right
+#define mod					1000000007
+#define inf					1e18
+#define ps(x,y)				fixed<<setprecision(y)<<x //converts x upto y decimal places
+#define mk(arr,n,type)		type *arr=new type[n];
+#define w(t)				int t; cin>>t; while(t--)
+mt19937						rng(chrono::steady_clock::now().time_since_epoch().count()); //shuffle(arr,arr+n,rng)
+#define sim	template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+
+/*sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);*/
+
+template < class c > struct rge { c b, e; };
+template < class c > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+template < class c > auto dud(c* x) -> decltype(cerr << *x, 0);
+template < class c > char dud(...);
+
+struct debug {
+	~debug() { cerr << endl; }
+	
+	//eni(!=) cerr << boolalpha << i; ris; }        Part 1
+	
+	template < class c > typename \
+	enable_if<sizeof dud<c>(0) != 1, debug&>::type operator<<(c i) {
+		cerr << boolalpha << i;
+		return * this;
+	}
+	
+	//eni(==) ris << range(begin(i), end(i)); }     Part 2
+	
+	template < class c > typename \
+	enable_if<sizeof dud<c>(0) == 1, debug&>::type operator<<(c i) {
+		return * this << range(begin(i), end(i)); 
+	}
+	
+	/*sim, class b dor(pair < b, c > d) {           Part 3
+		ris << "(" << d.first << ", " << d.second << ")";
+	}*/
+	
+	template < class c, class b > debug & operator << (pair < b, c > d) {
+		return * this << "(" << d.first << ", " << d.second << ")";
+	}
+	
+	/*sim dor(rge<c> d) {                           Part 4
+		*this << "[";
+		for (auto it = d.b; it != d.e; ++it)
+			*this << ", " + 2 * (it == d.b) << *it;
+		ris << "]";
+	}*/
+	
+	template < class c > debug & operator <<(rge<c> d) {
+		*this << "[";
+		for (auto it = d.b; it != d.e; ++it)
+			*this << ", " + 2 * (it == d.b) << *it;
+		return * this << "]";
+	}
+	
+};
+
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+
+void c_p_c(){
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+    		freopen("input.txt", "r", stdin);
+    		freopen("output.txt", "w", stdout);
+#endif
+}
+
+void pair_sum(vector<int> input_array, int target){
+	unordered_set <int> search;
+	for (int num : input_array){
+		int x = target-num;
+		if (search.find(x)!=search.end()){
+			cout << num << " " << x << endl;
+		}
+		search.insert(num);
+	} 
+}
+
+int32_t main()
+{
+	c_p_c();
+	
+	int n;
+	cin >> n;
+	
+	int element;
+	vector <int> input_array;
+	for (int i =0; i<n; i++){
+		cin >> element; input_array.push_back(element);
+	}
+	
+	int target;
+	cin >> target;
+	pair_sum(input_array, target);
+}
